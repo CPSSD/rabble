d32ce61d92af348e3c24d229f071255a3ba2e01f
CianLR <cian.ruane1@gmail.com>
2018-10-10 11:54:03 +0100

    Make run_build.sh work from anywhere

--- run_build.sh
+++ run_build.sh
@@ -2,20 +2,28 @@
 
 set -e
 
-echo "RM'ing $PWD/build_out"
-rm -rf $PWD/build_out
-mkdir $PWD/build_out
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+if [ -z "$REPO_ROOT" ]
+then
+  echo "Could not find root of repo"
+  exit 1
+fi
+
+BUILD_OUT="$REPO_ROOT/build_out"
+echo "RM'ing $BUILD_OUT"
+rm -rf $BUILD_OUT
+mkdir $BUILD_OUT
 
 echo "Creating build container image"
 docker build \
   --tag rabble_build:latest \
-  --file ./build_container/Dockerfile \
-  ./build_container
+  --file $REPO_ROOT/build_container/Dockerfile \
+  $REPO_ROOT/build_container
 
 echo "Running build container"
 docker run \
   --rm \
-  --volume $PWD:/repo \
+  --volume $REPO_ROOT:/repo \
   rabble_build:latest
 
 echo "Done build"
