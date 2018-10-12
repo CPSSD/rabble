23f1ba6fbd689918de133226d8c9326b40d015de
SailSlick <sailslickcode@gmail.com>
2018-10-12 11:58:14 +0100

    Add proto file generation to build scripts

--- build_container/build.sh
+++ build_container/build.sh
@@ -13,5 +13,13 @@ echo "Running build"
 echo "Building skinny server"
 go build -o build_out/skinny skinny/*.go
 
+echo "Building example go-python microservice"
+SRC_DIR=services/example_go_python_microservice
+protoc -I=$SRC_DIR \
+--python_out="services/example_go_python_microservice/python_docker" \
+--go_out="services/example_go_python_microservice/go_docker" \
+$SRC_DIR/greetingCard.proto
+go build -o build_out/example_go services/example_go_python_microservice/**/*.go
+
 echo "Building example binary"
 g++ services/example_microservice/main.cc -o build_out/example_ms
