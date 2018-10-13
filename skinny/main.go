fce14b5efde83ccb0185b0003f1266ad08aaa503
iandioch <iandioch11@gmail.com>
2018-10-13 15:36:41 +0000

    Add mux routing

--- skinny/main.go
+++ skinny/main.go
@@ -2,10 +2,26 @@ package main
 
 import (
     "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
 )
+
+type server struct {
+    router *mux.Router
+}
+
+func noopHandler(w http.ResponseWriter, r *http.Request) {
+    return
+}
+
+func (s *server) setupRoutes() {
+    s.router.HandleFunc("/", noopHandler)
+}
 
 func main() {
 	log.Printf("Starting skinny web server.\n")
-	for true {
-	}
+    r := mux.NewRouter()
+    server := &server{r}
+    server.setupRoutes()
 }
