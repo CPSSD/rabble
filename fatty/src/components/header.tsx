20e53efc8699833e469c511a0790840b4340a119
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2018-10-14 19:27:36 +0100

    Modify spelling of "center-brand" to "centre-brand"

--- fatty/src/components/header.tsx
+++ fatty/src/components/header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Header: React.StatelessComponent<{}> = () => {
   return (
-    <div className="pure-u-1 center-brand">
+    <div className="pure-u-1 centre-brand">
       <Link to="/" className="brand">Rabble</Link>
     </div>
   );
