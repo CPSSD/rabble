91ab5e1f0c94513333c4f4b98c41a8543d25d07a
iandioch <iandioch11@gmail.com>
2018-10-14 02:31:41 +0000

    80char columns, make comment first word be the var/func name, fix line split

--- skinny/main.go
+++ skinny/main.go
@@ -12,49 +12,49 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// serverWrapper encapsulates the dependencies and config values of
-// the server into one struct. Server endpoint handlers hang off of
-// this struct and can access their dependencies through it.
-// See https://medium.com/statuscode/how-i-write-go-http-services-after-seven-years-37c208122831
+// serverWrapper encapsulates the dependencies and config values of the server
+// into one struct. Server endpoint handlers hang off of this struct and can
+// access their dependencies through it. See
+// https://medium.com/statuscode/how-i-write-go-http-services-after-seven-years-37c208122831
 // for rationale and further explanation.
 type serverWrapper struct {
 	router *mux.Router
 	server *http.Server
-	// Specifies how long the server should wait when shutting down
-	// for existing connections to finish before forcing a shutdown.
+	// shutdownWait specifies how long the server should wait when shutting
+	// down for existing connections to finish before forcing a shutdown.
 	shutdownWait time.Duration
 }
 
-// Returns a http.HandlerFunc with a 501 Not Implemented error.
+// handleNotImplemented returns a http.HandlerFunc with a 501 Not Implemented
+// error.
 func (s *serverWrapper) handleNotImplemented() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w,
+		http.Error(
+			w,
 			http.StatusText(http.StatusNotImplemented),
 			http.StatusNotImplemented)
 	}
 }
 
 // setupRoutes specifies the routing of all endpoints on the server.
-// Centralised routing config allows easier debugging of a specific
-// endpoint, as the code handling it can be looked up here.
-// The server uses mux for routing. See instructions and examples for
-// mux at https://www.gorillatoolkit.org/pkg/mux .
-// TODO(iandioch): Move setupRoutes() to its own file if/when it gets
-// too big.
+// Centralised routing config allows easier debugging of a specific endpoint,
+// as the code handling it can be looked up here.
+// The server uses mux for routing. See instructions and examples for mux at
+// https://www.gorillatoolkit.org/pkg/mux .
+// TODO(iandioch): Move setupRoutes() to its own file if/when it gets too big.
 func (s *serverWrapper) setupRoutes() {
 	log.Printf("Setting up routes on skinny server.\n")
 	r := s.router
 	r.HandleFunc("/", s.handleNotImplemented())
 }
 
-// Set up all necessary individual parts of the server wrapper,
-// and return one that is ready to run.
+// buildServerWrapper sets up all necessary individual parts of the server
+// wrapper, and returns one that is ready to run.
 func buildServerWrapper() *serverWrapper {
 	r := mux.NewRouter()
 	srv := &http.Server{
 		Addr: "0.0.0.0:1916",
-		// Important to specify timeouts in order to prevent
-		// Slowloris attacks.
+		// Important to specify timeouts in order to prevent Slowloris attacks.
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
@@ -71,8 +71,8 @@ func main() {
 
 	// The following code is partially taken from this link:
 	// https://github.com/gorilla/mux#graceful-shutdown
-	// It lays out a way for a server to gracefully shutdown
-	// when a SIGINT (Ctrl+C) or SIGTERM (kill) is received.
+	// It lays out a way for a server to gracefully shutdown when a SIGINT
+	// (Ctrl+C) or SIGTERM (kill) is received.
 	// See also: https://gobyexample.com/signals
 
 	go func() {
@@ -81,8 +81,8 @@ func main() {
 		}
 	}()
 
-	// Accept graceful shutdowns when quit via SIGINT or SIGTERM.
-	// Other signals (eg. SIGKILL, SIGQUIT) will not be caught.
+	// Accept graceful shutdowns when quit via SIGINT or SIGTERM. Other signals
+	// (eg. SIGKILL, SIGQUIT) will not be caught.
 	// Docker sends a SIGTERM on shutdown.
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
@@ -92,8 +92,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), s.shutdownWait)
 	defer cancel()
 
-	// Waits for active connections to terminate, or until it hits
-	// the timeout.
+	// Waits for active connections to terminate, or until it hits the timeout.
 	s.server.Shutdown(ctx)
 
 	log.Printf("Stopping skinny server.\n")
