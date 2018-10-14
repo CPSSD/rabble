b712a22f06e41e8e7b2117edce3b2de2573c0891
SailSlick <sailslickcode@gmail.com>
2018-10-14 09:44:32 +0100

    Run gofmt

--- services/example_go_python_microservice/go/gRPC_example.go
+++ services/example_go_python_microservice/go/gRPC_example.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net"
-  "golang.org/x/net/context"
-  "google.golang.org/grpc"
-  protobuf "greetingCard"
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	protobuf "greetingCard"
+	"log"
+	"net"
 )
 
 type server struct{}
 
 func (s *server) GetGreetingCard(ctx context.Context, in *protobuf.GreetingCard) (*protobuf.AcknowledgmentCard, error) {
-  fmt.Println("Got card from " + in.Sender)
-  return &protobuf.AcknowledgmentCard{ Letter:"Thank you for the card, " + in.Sender }, nil
+	fmt.Println("Got card from " + in.Sender)
+	return &protobuf.AcknowledgmentCard{Letter: "Thank you for the card, " + in.Sender}, nil
 }
 
 func main() {
-  lis, err := net.Listen("tcp", "8000")
-  if err != nil {
-    log.Fatalf("Failed to listen: %v", err)
-  }
-  grpcServer := grpc.NewServer()
-  protobuf.RegisterGreetingCardsServer(grpcServer, &server{})
-  grpcServer.Serve(lis)
+	lis, err := net.Listen("tcp", "8000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	protobuf.RegisterGreetingCardsServer(grpcServer, &server{})
+	grpcServer.Serve(lis)
 }
