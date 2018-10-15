a313e4918e002e89f4fccf027237a937852ed933
iandioch <iandioch11@gmail.com>
2018-10-15 12:43:48 +0000

    Add test for handlenotimplemented

---
+++ skinny/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "net/http"
+    "net/http/httptest"
+    "testing"
+)
+
+func newTestServerWrapper() *serverWrapper {
+    // TODO(iandioch): Fake/mock instead of using real dependencies
+    return buildServerWrapper()
+}
+
+func TestHandleNotImplemented(t *testing.T) {
+    req, _ := http.NewRequest("GET", "/", nil)
+    res := httptest.NewRecorder()
+    newTestServerWrapper().router.ServeHTTP(res, req);
+
+    if res.Code != http.StatusNotImplemented {
+        t.Errorf("Expected 501 Not Implemented, got %#v", res.Code)
+    }
+    if res.Body.String() != "Not Implemented\n" {
+        t.Errorf("Expected 'Not Implemented' body, got %#v", res.Body.String())
+    }
+}
