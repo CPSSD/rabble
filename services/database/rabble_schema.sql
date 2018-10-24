3cf650f84503b8f6c6e1a7d1cf5a8a086a4b2dcc
iandioch <iandioch11@gmail.com>
2018-10-24 22:14:39 +0000

    add schema

--- services/database/rabble_schema.sql
+++ services/database/rabble_schema.sql
@@ -5,4 +5,17 @@ CREATE TABLE IF NOT EXISTS posts (
   body              text    NOT NULL,
   creation_datetime integer NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS local_users (
+  handle            text    PRIMARY KEY,
+  display_name      text    NOT NULL
+);
+
+/* Table for a local follower following a local big_cheese account.
+   TODO(iandioch): Look into using foreign keys */
+CREATE TABLE IF NOT EXISTS local_follows (
+  follower          text    PRIMARY KEY,
+  big_cheese        text    NOT NULL
+);
+
 /* Add other tables here */
