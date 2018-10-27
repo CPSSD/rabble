19f2ee76daa9d687c660aa7e1aa455d24d60de4e
iandioch <iandioch11@gmail.com>
2018-10-27 20:15:53 +0000

    add util_test.py

---
+++ services/follows/util_test.py
@@ -0,0 +1,39 @@
+import logging
+import unittest
+
+from util import Util
+
+class UtilTest(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.util = Util(self.logger)
+
+    def test_parse_local_username(self):
+        a, b = self.util.parse_username('admin')
+        self.assertEqual(a, 'admin')
+        self.assertIsNone(b)
+
+    def test_parse_foreign_username(self):
+        a, b = self.util.parse_username('cianlr@neopets.com')
+        self.assertEqual(a, 'cianlr')
+        self.assertEqual(b, 'neopets.com')
+
+    def test_parse_prefixed_local_username(self):
+        a, b = self.util.parse_username('@admin')
+        self.assertEqual(a, 'admin')
+        self.assertIsNone(b)
+
+    def test_parse_prefixed_foreign_username(self):
+        a, b = self.util.parse_username('@cianlr@neopets.com')
+        self.assertEqual(a, 'cianlr')
+        self.assertEqual(b, 'neopets.com')
+
+    def test_parse_bad_username(self):
+        with self.assertLogs(self.logger, level='WARNING'):
+            a, b = self.util.parse_username('a@b@c')
+            self.assertIsNone(a)
+            self.assertIsNone(b)
+
+
+if __name__ == '__main__':
+    unittest.main()
