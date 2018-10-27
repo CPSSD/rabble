c9419bd09b266e1b9b2d9ab0203ab05cee8147cf
iandioch <iandioch11@gmail.com>
2018-10-27 02:18:27 +0000

    s/database/follows

--- services/follows/receive_follow.py
+++ services/follows/receive_follow.py
@@ -9,5 +9,5 @@ class ReceiveFollowServicer:
     def ReceiveFollowRequest(self, request, context):
         resp = follows_pb2.FollowResponse()
         self._logger.info('Receive follow request.')
-        resp.result_type = database_pb2.FollowResponse.OK
+        resp.result_type = follows_pb2.FollowResponse.OK
         return response
