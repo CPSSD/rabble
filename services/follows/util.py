b51e6498d5b3ccf249a867d81918c6a282dfecaf
iandioch <iandioch11@gmail.com>
2018-10-28 18:39:43 +0000

    allow getting user with an id instead of host and handle

--- services/follows/util.py
+++ services/follows/util.py
@@ -33,14 +33,20 @@ class Util:
         # TODO(iandioch): Respond to errors.
         return insert_resp
 
-    def get_user_from_db(self, handle, host, attempt_number=0):
+    def get_user_from_db(self,
+                        handle=None,
+                        host=None,
+                        global_id=None,
+                        attempt_number=0):
         if attempt_number > MAX_FIND_RETRIES:
             self._logger.error('Retried query too many times.')
             return None
-        self._logger.debug('User %s@%s requested from database', handle, host)
+        self._logger.debug('User %s@%s (id %s) requested from database',
+                           handle, host, global_id)
         user_entry = database_pb2.UsersEntry(
             handle=handle,
-            host=host
+            host=host,
+            global_id=global_id
         )
         find_req = database_pb2.UsersRequest(
             request_type=database_pb2.UsersRequest.FIND,
@@ -48,16 +54,22 @@ class Util:
         )
         find_resp = self._db.Users(find_req)
         if len(find_resp.results) == 0:
-            self._logger.warning('No user %s@%s found', handle, host)
+            self._logger.warning('No user %s@%s (id %s) found',
+                                 handle, host, global_id)
+            if global_id is not None:
+                # Should not try to create a user and hope it has this ID.
+                return None
             self._create_user_in_db(user_entry)
             return self.get_user_from_db(handle, host,
                                          attempt_number=attempt_number+1)
         elif len(find_resp.results) == 1:
-            self._logger.debug('Found user %s@%s from database', handle, host)
+            self._logger.debug('Found user %s@%s (id %s) from database',
+                               handle, host, global_id)
             return find_resp.results[0]
         else:
-            self._logger.error('> 1 user found in database for %s@%s' +
-                               ', returning first one.', handle, host)
+            self._logger.error('> 1 user found in database for %s@%s (id %s)' +
+                               ', returning first one.',
+                               handle, host, global_id)
             return find_resp.results[0]
 
     def create_follow_in_db(self, follower_id, followed_id):
@@ -93,4 +105,4 @@ class Util:
         if follow_resp.result_type == database_pb2.DbFollowResponse.ERROR:
             self._logger.error('Could not add follow to database: %s',
                                follow_resp.error)
-        return follow_Resp
+        return follow_resp
