e67d053cc8646c4105bac2eb0fa25cb04a6e5e75
iandioch <iandioch11@gmail.com>
2018-10-28 22:17:20 +0000

    find using match instead of entry

--- services/follows/util.py
+++ services/follows/util.py
@@ -92,15 +92,15 @@ class Util:
 
     def get_follows(self, follower_id=None, followed_id=None):
         self._logger.debug('Finding follows <User ID %s following User ID %s>',
-                           ('*' if follower_id is None else str(follower_id))
-                           ('*' if followed_id is None else str(followed_id))
+                           ('*' if (follower_id is None) else str(follower_id)),
+                           ('*' if (followed_id is None) else str(followed_id)))
         follow_entry=database_pb2.Follow(
             follower=follower_id,
             followed=followed_id
         )
         follow_req=database_pb2.DbFollowRequest(
             request_type=database_pb2.DbFollowRequest.FIND,
-            entry=follow_entry
+            match=follow_entry
         )
         follow_resp=self._db.Follow(follow_req)
         if follow_resp.result_type == database_pb2.DbFollowResponse.ERROR:
@@ -109,6 +109,7 @@ class Util:
         return follow_resp
 
     def convert_db_user_to_follow_user(self, db_user, follow_user):
+        self._logger.warning('Trying to convert %s %s', db_user.handle, db_user.host)
         try:
             follow_user.handle=db_user.handle
             follow_user.host=db_user.host
