3cc308dc0286f53ff30b9ab88c9d555eb240dfc8
iandioch <iandioch11@gmail.com>
2018-10-28 15:06:39 +0000

    add users find db method

--- services/database/users_servicer.py
+++ services/database/users_servicer.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+import util
+
 import database_pb2
 
 
@@ -10,7 +12,28 @@ class UsersDatabaseServicer:
         self._logger = logger
         self._users_type_handlers = {
             database_pb2.UsersRequest.INSERT: self._users_handle_insert,
+            database_pb2.UsersRequest.FIND: self._users_handle_find.
         }
+
+    def _db_tuple_to_entry(self, tup, entry):
+        if len(tup) != 4:
+            self._logger.warning(
+                "Error converting tuple to UsersEntry: " +
+                "Wrong number of elements " + str(tup))
+            return False
+        try:
+            # You'd think there'd be a better way.
+            entry.handle = tup[0]
+            entry.display_name = tup[1]
+            entry.host = tup[2]
+            entry.global_id = tup[3]
+        except Exception as e:
+            self._logger.warning(
+                "Error converting tuple to UsersEntry: " +
+                str(e))
+            return False
+        return True
+
 
     def Users(self, request, context):
         response = database_pb2.UsersResponse()
@@ -34,3 +57,21 @@ class UsersDatabaseServicer:
             resp.error = str(e)
             return
         resp.result_type = database_pb2.UsersResponse.OK
+
+    def _users_handle_find(self, req, resp):
+        filter_clause, values = util.entry_to_filter(req.match)
+        try:
+            if not filter_clause:
+                res = self._db.execute('SELECT * FROM users')
+            else:
+                res = self._db.execute(
+                    'SELECT * FROM users WHERE ' + filter_clause,
+                    *values)
+        except sqlite3.Error as e:
+            resp.result_type = database_pb2.UsersResponse.ERROR
+            resp.error = str(e)
+            return
+        resp.result_type = database_pb2.UsersResponse.OK
+        for tup in res:
+            if not self._db_tuple_to_entry(tup, resp.results.add()):
+                del resp.results[-1]
