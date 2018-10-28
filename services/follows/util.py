094482e9bcb1d07d26adae28f96068e8d0a0f114
iandioch <iandioch11@gmail.com>
2018-10-28 14:48:13 +0000

    add find req to follows/util.py

--- services/follows/util.py
+++ services/follows/util.py
@@ -1,7 +1,10 @@
+import database_pb2
+
 class Util:
 
-    def __init__(self, logger):
+    def __init__(self, logger, db_stub):
         self._logger = logger
+        self._db = db_stub
 
     def parse_username(self, username):
         username = username.lstrip('@')
@@ -15,3 +18,24 @@ class Util:
         # Username is incorrect/malicious/etc.
         self._logger.warning('Couldn\'t parse username %s', username)
         return None, None
+
+    def get_user_from_db(self, handle, host):
+        self._logger.debug('User %s@%s requested from database', handle, host)
+        user_entry = database_pb2.UsersEntry(
+            handle = handle,
+            host = host
+        )
+        find_req = database_pb2.UsersRequest(
+            request_type=database_pb2.UsersRequest.FIND,
+            entry = user_entry
+        )
+        find_resp = self._db.Users(find_req)
+        if len(find_resp.results) == 0:
+            self._logger.warning('No user %s@%s found', handle, host)
+        elif len(find_resp.results) == 1:
+            self._logger.debug('Found user %s@%s from database', handle, host)
+            return find_resp.results[0]
+        else:
+            self._logger.error('> 1 user found in database for %s@%s' + \
+                               ', returning first one.', handle, host)
+            return find_resp.results[0]
