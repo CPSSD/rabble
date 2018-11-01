97873679813fd354d026ee419143c21305451914
iandioch <iandioch11@gmail.com>
2018-11-01 04:07:33 +0000

    Fix tests

--- chump/src/components/feed.tsx
+++ chump/src/components/feed.tsx
@@ -43,10 +43,14 @@ export class Feed extends React.Component<{}, IFeedState> {
           <div className="pure-u-1-24"/>
           <div className="pure-u-3-24">
             <div className="author-about">
-              <img src="https://qph.fs.quoracdn.net/main-qimg-8aff684700be1b8c47fa370b6ad9ca13.webp" className="author-thumbnail"></img>
-              <Link to="/" className="author-displayname">{e.author}</Link><br />
+              <img
+                src="https://qph.fs.quoracdn.net/main-qimg-8aff684700be1b8c47fa370b6ad9ca13.webp"
+                className="author-thumbnail"
+              />
+              <Link to="/" className="author-displayname">{e.author}</Link><br/>
               <Link to="/" className="author-handle">@{e.author}</Link>
-              <p className="author-bio">Nowadays everybody wanna talk like they got something to say. But nothing comes out when they move their lips; just a bunch of gibberish.</p>
+              <p className="author-bio">Nowadays everybody wanna talk like they got something to say.
+              But nothing comes out when they move their lips; just a bunch of gibberish.</p>
             </div>
           </div>
         </div>
