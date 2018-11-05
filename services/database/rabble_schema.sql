a25e01f38077f6e65e77d2e27eec6548424c8646
iandioch <iandioch11@gmail.com>
2018-11-05 16:20:50 +0000

    change post author in database microservice

--- services/database/rabble_schema.sql
+++ services/database/rabble_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS posts (
   global_id         text    PRIMARY KEY,
-  author            text    NOT NULL,
+  author_id         integer NOT NULL,
   title             text    NOT NULL,
   body              text    NOT NULL,
   creation_datetime integer NOT NULL
