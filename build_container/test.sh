98e5706e3563d52effb1837fad2578234b93c009
iandioch <iandioch11@gmail.com>
2018-11-06 17:05:31 +0000

    Uncancel FE tests

--- build_container/test.sh
+++ build_container/test.sh
@@ -4,7 +4,7 @@ set -e
 
 cd /repo
 
-#cd chump/ && npm run lint && npm run test && cd ..
+cd chump/ && npm run lint && npm run test && cd ..
 
 # once we clean up the go build management, we should just be
 # able to run go test /go/src/github.com/cppsd/rabble/...
