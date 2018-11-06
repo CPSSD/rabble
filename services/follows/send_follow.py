345e796406e54b3506f4114f30ff08618feee495
iandioch <iandioch11@gmail.com>
2018-11-06 16:50:01 +0000

    Change references to util in follows MS

--- services/follows/send_follow.py
+++ services/follows/send_follow.py
@@ -4,9 +4,10 @@ from proto import follows_pb2
 
 class SendFollowServicer:
 
-    def __init__(self, logger, util, database_stub):
+    def __init__(self, logger, util, users_util, database_stub):
         self._logger = logger
         self._util = util
+        self._users_util = users_util
         self._database_stub = database_stub
 
     def SendFollowRequest(self, request, context):
@@ -27,7 +28,7 @@ class SendFollowServicer:
             return resp
 
         # Get user IDs for follow.
-        follower_entry = self._util.get_user_from_db(
+        follower_entry = self._users_util.get_user_from_db(
             handle=from_handle, host=from_instance)
         if follower_entry is None:
             error = 'Could not find or create user {}@{}'.format(from_handle,
@@ -36,7 +37,7 @@ class SendFollowServicer:
             resp.result_type = follows_pb2.FollowResponse.ERROR
             resp.error = error
             return resp
-        followed_entry = self._util.get_user_from_db(handle=to_handle,
+        followed_entry = self._users_util.get_user_from_db(handle=to_handle,
                                                      host=to_instance)
         if followed_entry is None:
             error = 'Could not find or create user {}@{}'.format(to_handle,
