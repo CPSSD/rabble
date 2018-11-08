e04af8334ebcc8c92c05c3a815681b81c8cbde8e
SailSlick <sailslickcode@gmail.com>
2018-11-08 16:20:51 +0000

    Link article service with create service

--- services/article/main.py
+++ services/article/main.py
@@ -10,6 +10,7 @@ from utils.logger import get_logger
 from servicer import ArticleServicer
 from proto import article_pb2_grpc
 from proto import database_pb2_grpc
+from proto import create_pb2_grpc
 
 
 def get_args():
@@ -29,16 +30,28 @@ def get_db_channel():
     return grpc.insecure_channel(db_service_address)
 
 
+def get_create_channel():
+    create_service_host = os.environ["CREATE_SERVICE_HOST"]
+    if not create_service_host:
+        print("Please set CREATE_SERVICE_HOST env variable")
+        sys.exit(1)
+    create_service_address = create_service_host + ":1922"
+    return grpc.insecure_channel(create_service_address)
+
+
 def main():
     args = get_args()
     logger = get_logger("article_service", args.v)
     logger.info("Creating db connection")
     db_channel = get_db_channel()
     db_stub = database_pb2_grpc.DatabaseStub(db_channel)
+    logger.info("Creating create connection")
+    create_channel = get_create_channel()
+    create_stub = create_pb2_grpc.CreateStub(create_channel)
     logger.info("Creating article server")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     article_pb2_grpc.add_ArticleServicer_to_server(
-        ArticleServicer(db_stub, logger),
+        ArticleServicer(create_stub, db_stub, logger),
         server
         )
     server.add_insecure_port('0.0.0.0:1601')
@@ -49,6 +62,7 @@ def main():
             time.sleep(60 * 60 * 24)  # One day
     except KeyboardInterrupt:
         db_channel.close()
+        create_channel.close()
         pass
 
 if __name__ == '__main__':
