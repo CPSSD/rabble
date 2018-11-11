e8bc68d90d88fce8a3a983a1116b13ff9c6c7b22
CianLR <cian.ruane1@gmail.com>
2018-11-11 14:54:59 +0000

    Seperate Post rendering into seperate component

--- chump/src/components/feed.tsx
+++ chump/src/components/feed.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 
+import { Post } from "./post";
 import { GetPublicPosts, IBlogPost } from "../models/posts";
 
 interface IFeedState {
@@ -31,30 +32,7 @@ export class Feed extends React.Component<{}, IFeedState> {
 
   public renderPosts() {
     return this.state.publicBlog.map((e: IBlogPost, i: number) => {
-      // TODO(devoxel): Replace dangerouslySetInnerHTML with a safer option
-      return (
-        <div className="pure-g" key={i}>
-          <div className="pure-u-5-24"/>
-          <div className="pure-u-10-24">
-            <p className="article-byline">Published 1st January 1970</p>
-            <h3 className="article-title">{e.title}</h3>
-            <p className="article-body" dangerouslySetInnerHTML={{ __html: e.body }}/>
-          </div>
-          <div className="pure-u-1-24"/>
-          <div className="pure-u-3-24">
-            <div className="author-about">
-              <img
-                src="https://qph.fs.quoracdn.net/main-qimg-8aff684700be1b8c47fa370b6ad9ca13.webp"
-                className="author-thumbnail"
-              />
-              <Link to="/" className="author-displayname">{e.author}</Link><br/>
-              <Link to="/" className="author-handle">@{e.author}</Link>
-              <p className="author-bio">Nowadays everybody wanna talk like they got something to say.
-              But nothing comes out when they move their lips; just a bunch of gibberish.</p>
-            </div>
-          </div>
-        </div>
-      );
+      return (<Post blogPost={e} index={i} />);
     });
   }
 
---
+++ chump/src/components/post.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+import { IBlogPost } from "../models/posts";
+
+interface IPostProps {
+  blogPost: IBlogPost;
+  index: number;
+}
+
+export class Post extends React.Component<IPostProps, {}> {
+  constructor(props: IPostProps) {
+    super(props);
+    this.state = { };
+  }
+
+  public render() {
+    // TODO(devoxel): Replace dangerouslySetInnerHTML with a safer option
+    return (
+      <div className="pure-g" key={this.props.index}>
+        <div className="pure-u-5-24"/>
+        <div className="pure-u-10-24">
+          <p className="article-byline">Published 1st January 1970</p>
+          <h3 className="article-title">{this.props.blogPost.title}</h3>
+          <p className="article-body" dangerouslySetInnerHTML={{ __html: this.props.blogPost.body }}/>
+        </div>
+        <div className="pure-u-1-24"/>
+        <div className="pure-u-3-24">
+          <div className="author-about">
+            <img
+              src="https://qph.fs.quoracdn.net/main-qimg-8aff684700be1b8c47fa370b6ad9ca13.webp"
+              className="author-thumbnail"
+            />
+            <Link to="/" className="author-displayname">{this.props.blogPost.author}</Link><br/>
+            <Link to="/" className="author-handle">@{this.props.blogPost.author}</Link>
+            <p className="author-bio">Nowadays everybody wanna talk like they got something to say.
+            But nothing comes out when they move their lips; just a bunch of gibberish.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
