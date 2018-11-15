11df8e6dbc6af681e0d4145f5d545bec3b4a780f
CianLR <cian.ruane1@gmail.com>
2018-11-15 18:21:46 +0000

    Add LoginHandler and tests

--- services/users/login.py
+++ services/users/login.py
@@ -1,3 +1,5 @@
+import bcrypt
+
 from proto import users_pb2
 from proto import database_pb2
 
@@ -6,6 +8,49 @@ class LoginHandler:
         self._logger = logger
         self._db_stub = db_stub
 
+    def _check_password(self, pw, pw_hash):
+        try:
+            return bcrypt.checkpw(pw.encode('utf-8'),
+                                  pw_hash.encode('utf-8'))
+        except ValueError as e:
+            self._logger.warning(
+                "Password hash in DB '%s' caused an error: %s",
+                pw_hash, str(e))
+            return False # Invalid salt.
+
     def Login(self, request, context):
-        pass
+        find_request = database_pb2.UsersRequest(
+            request_type=database_pb2.UsersRequest.FIND,
+            match=database_pb2.UsersEntry(
+                handle=request.handle,
+            ),
+        )
+        db_resp = self._db_stub.Users(find_request)
+        # Check for errors
+        if db_resp.result_type != database_pb2.UsersResponse.OK:
+            self._logger.warning("Error getting user from DB: %s",
+                                 db_resp.error)
+            return users_pb2.LoginResponse(
+                result=users_pb2.LoginResponse.ERROR,
+                error_details=db_resp.error,
+            )
+        elif len(db_resp.results) != 1:
+            self._logger.warning(
+                "Got %d users matching handle %s, expecting 1",
+                len(db_resp.results), request.handle)
+            return users_pb2.LoginResponse(
+                result=users_pb2.LoginResponse.ERROR,
+                error_details="Got wrong number of users matching query",
+            )
+        # Check password
+        if not self._check_password(request.password,
+                                    db_resp.results[0].password):
+            self._logger.info("ACCESS DENIED for user %s", request.handle)
+            return users_pb2.LoginResponse(
+                result=users_pb2.LoginResponse.DENIED,
+            )
+        self._logger.info("ACCESS GRANTED for user %s", request.handle)
+        return users_pb2.LoginResponse(
+            result=users_pb2.LoginResponse.ACCEPTED,
+        )
 
