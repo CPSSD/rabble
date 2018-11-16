65bb17c8731d7898542401555356177d2af2c1c4
iandioch <iandioch11@gmail.com>
2018-11-16 03:01:49 +0000

    add tests

--- services/activities/follow/test_send_follow_servicer.py
+++ services/activities/follow/test_send_follow_servicer.py
@@ -8,6 +8,23 @@ class SendFollowServicerTest(unittest.TestCase):
     def setUp(self):
         self.servicer = SendFollowServicer(Mock())
 
+    def test_build_actor(self):
+        self.assertEqual(self.servicer._build_actor('a', 'b.com'),
+                         'b.com/@a')
+
+    def test_build_inbox_url(self):
+        self.assertEqual(self.servicer._build_inbox_url('a', 'b.com'),
+                         'b.com/ap/@a/inbox')
+
+    def test_build_activity(self):
+        e = self.servicer._build_activity('FOLLOWER', 'FOLLOWED')
+        self.assertEqual(e['@context'],
+                         'https://www.w3.org/ns/activitystreams')
+        self.assertEqual(e['type'], 'Follow')
+        self.assertEqual(e['actor'], 'FOLLOWER')
+        self.assertEqual(e['object'], 'FOLLOWED')
+        self.assertEqual(e['to'], ['FOLLOWED'])
+
     def test_SendFollowActivity(self):
         resp = self.servicer.SendFollowActivity(None, None)
         self.assertEqual(resp.result_type, 
