d00408367720a252ea3272145555916733dbb309
iandioch <iandioch11@gmail.com>
2018-11-16 00:40:48 +0000

    Add no-op receiver

---
+++ services/activities/follow/receive_follow_servicer.py
@@ -0,0 +1,13 @@
+import json
+
+from proto import s2s_follow_pb2
+
+class ReceiveFollowServicer:
+
+    def __init__(self, logger):
+        self._logger = logger
+
+    def ReceiveFollowActivity(self, req, context):
+        resp = s2s_follow_pb2.FollowActivityResponse()
+        resp.result_type = s2s_follow_pb2.FollowActivityResponse.OK
+        return resp
--- services/activities/follow/servicer.py
+++ services/activities/follow/servicer.py
@@ -1,4 +1,5 @@
 from send_follow_servicer import SendFollowServicer
+from receive_follow_servicer import ReceiveFollowServicer
 from proto import s2s_follow_pb2_grpc
 
 
@@ -9,3 +10,5 @@ class FollowServicer(s2s_follow_pb2_grpc.S2SFollowServicer):
 
         send_follow_servicer = SendFollowServicer(logger)
         self.SendFollowActivity = send_follow_servicer.SendFollowActivity
+        receive_follow_servicer = ReceiveFollowServicer(logger)
+        self.SendFollowActivity = receive_follow_servicer.ReceiveFollowActivity
