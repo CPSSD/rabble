213836f89d9f4171c217c25c1cf627b841279c03
iandioch <iandioch11@gmail.com>
2018-11-16 00:31:32 +0000

    Add no-op send_follow_servicer

--- services/activities/follow/send_follow_servicer.py
+++ services/activities/follow/send_follow_servicer.py
@@ -1,4 +1,3 @@
-import urllib3
 import json
 
 from proto import s2s_follow_pb2
@@ -7,7 +6,6 @@ class SendFollowServicer:
 
     def __init__(self, logger):
         self._logger = logger
-        self._client = urllib3.PoolManager()
 
     def SendFollowActivity(self, req, context):
         resp = s2s_follow_pb2.FollowActivityResponse()
