f60510c715382990ca37b4f06a40d622a28a1e47
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2018-11-18 13:54:48 +0000

    Add foreign populate

---
+++ script/foreign_populate.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+if [ -z "$REPO_ROOT" ]
+then
+  echo "Could not find root of repo"
+  exit 1
+fi
+
+. $REPO_ROOT/script/rabble_utils.sh
+
+LOCAL=localhost:1916
+FOREIGN=localhost:1917
+
+RLOCAL=skinny:1916
+RFOREIGN=skinny2:1917
+
+create_user $FOREIGN david
+
+follow $FOREIGN david michael@$RLOCAL
