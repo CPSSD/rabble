36943ec1dc85da29d7efe2ed279ce698642fac80
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2018-11-19 11:14:47 +0000

    Add some buttons

--- chump/src/components/header.tsx
+++ chump/src/components/header.tsx
@@ -12,11 +12,15 @@ export const Header: React.StatelessComponent<IHeaderProps> = (props) => {
       <Link to="/login" className="pure-menu-link">Login</Link>
     </li>
   );
+
   let RegisterOrLogout = (
     <li className="pure-menu-item">
       <Link to="/register" className="pure-menu-link">Register</Link>
     </li>
   );
+
+  let Feed: JSX.Element | boolean = false;
+  let Follow: JSX.Element | boolean = false;
 
   if (props.username !== "") {
     Login = (
@@ -34,12 +38,24 @@ export const Header: React.StatelessComponent<IHeaderProps> = (props) => {
         <Link to="/logout" className="pure-menu-link">Logout</Link>
       </li>
     );
+    Feed = (
+        <li className="pure-menu-item">
+          <Link to="/feed" className="pure-menu-link">Feed</Link>
+        </li>
+    );
+    Follow = (
+        <li className="pure-menu-item">
+          <Link to="/follow" className="pure-menu-link">Follow</Link>
+        </li>
+    );
   }
+
+
 
   return (
     <div className="pure-g topnav">
       <div className="pure-u-5-24"/>
-      <div className="pure-u-10-24 centre-brand">
+      <div className="pure-u-1-4 centre-brand">
         <Link to="/" className="brand">Rabble</Link>
       </div>
       <div className="pure-u-1-24"/>
@@ -49,6 +65,8 @@ export const Header: React.StatelessComponent<IHeaderProps> = (props) => {
             <li className="pure-menu-item">
               <Link to="/write" className="pure-menu-link">Write</Link>
             </li>
+            {Follow}
+            {Feed}
             {Login}
             {RegisterOrLogout}
           </ul>
