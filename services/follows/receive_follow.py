45fda10091d18a7b00cac1303f82b595e0fede04
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2018-11-28 17:03:55 +0000

    Refactor body of recieve
    This just moves away the validation and user finding parts to a seperate
    function. It's a little bit easier to understand whats going on, and
    will be easier to add functionality to.

--- services/follows/receive_follow.py
+++ services/follows/receive_follow.py
@@ -10,14 +10,25 @@ class ReceiveFollowServicer:
         self._users_util = users_util
         self._database_stub = database_stub
 
-    def ReceiveFollowRequest(self, request, context):
-        resp = follows_pb2.FollowResponse()
+    def _validate_and_get_users(self, resp, request):
+        """
+        _validate_and_get_users validates request fields and finds the
+        requested users. It takes as an argument the response field, and
+        handles errors if they should happen.
+        
+        Returns:
+          local_user, foreign_user
+          If either is None, then the request has failed and the error has
+          already been written.
+        """
         from_handle, from_host = request.follower_handle, request.follower_host
         to_handle = request.followed
+
         self._logger.info('%s@%s has requested to follow %s (local user).',
                           from_handle,
                           from_host,
                           to_handle)
+
         if from_host is None or from_host == '':
             error = \
                 'No host associated with foreign user {}'.format(from_handle)
@@ -25,7 +36,7 @@ class ReceiveFollowServicer:
             self._logger.error('Aborting follow request')
             resp.result_type = follows_pb2.FollowResponse.ERROR
             resp.error = error
-            return resp
+            return None, None
 
         local_user = self._users_util.get_user_from_db(handle=to_handle,
                                                        host=None)
@@ -34,7 +45,7 @@ class ReceiveFollowServicer:
             self._logger.error(error)
             resp.result_type = follows_pb2.FollowResponse.ERROR
             resp.error = error
-            return resp
+            return None, None
 
         foreign_user = self._users_util.get_or_create_user_from_db(
             handle=from_handle,
@@ -45,6 +56,14 @@ class ReceiveFollowServicer:
             self._logger.error(error)
             resp.result_type = follows_pb2.FollowResponse.ERROR
             resp.error = error
+            return local_user, None
+        return local_user, foreign_user
+        
+
+    def ReceiveFollowRequest(self, request, context):
+        resp = follows_pb2.FollowResponse()
+        foreign_user, local_user = self._validate_and_get_users(resp, request)
+        if foreign_user == None or local_user == None:
             return resp
 
         self._logger.info('User ID %d has requested to follow User ID %d',
