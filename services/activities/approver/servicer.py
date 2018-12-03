11754ea2dbe167048ed14e1d088c73494e0c106b
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2018-12-03 15:37:12 +0000

    Add ActivityUtil to approval

--- services/activities/approver/servicer.py
+++ services/activities/approver/servicer.py
@@ -3,10 +3,10 @@ from receive import ReceiveApprovalServicer
 from services.proto import approver_pb2_grpc
 
 class ApproverServicer(approver_pb2_grpc.ApproverServicer):
-    def __init__(self, logger, db_stub):
+    def __init__(self, logger, db_stub, activ_util):
         self._logger = logger
 
-        send = SendApprovalServicer(logger)
+        send = SendApprovalServicer(logger, activ_util)
         self.SendApproval = send.SendApproval
         receive = ReceiveApprovalServicer(logger)
         self.ReceiveApproval = receive.ReceiveApproval
