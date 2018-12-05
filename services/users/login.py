fdab5cbafba0f9b08bca8c131e3a70e839ce5ab2
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2018-12-05 18:15:33 +0000

    Refactor most of Login into a util function

--- services/users/login.py
+++ services/users/login.py
@@ -1,58 +1,31 @@
-import bcrypt
-
 from services.proto import users_pb2
-from services.proto import database_pb2
+
+from util import get_user_and_check_pw
 
 class LoginHandler:
     def __init__(self, logger, db_stub):
         self._logger = logger
         self._db_stub = db_stub
 
-    def _check_password(self, pw, pw_hash):
-        try:
-            return bcrypt.checkpw(pw.encode('utf-8'),
-                                  pw_hash.encode('utf-8'))
-        except ValueError as e:
-            self._logger.warning(
-                "Password hash in DB '%s' caused an error: %s",
-                pw_hash, str(e))
-            return False # Invalid salt.
-
     def Login(self, request, context):
-        find_request = database_pb2.UsersRequest(
-            request_type=database_pb2.UsersRequest.FIND,
-            match=database_pb2.UsersEntry(
-                handle=request.handle,
-            ),
-        )
-        db_resp = self._db_stub.Users(find_request)
-        # Check for errors
-        if db_resp.result_type != database_pb2.UsersResponse.OK:
-            self._logger.warning("Error getting user from DB: %s",
-                                 db_resp.error)
-            return users_pb2.LoginResponse(
-                result=users_pb2.LoginResponse.ERROR,
-                error=db_resp.error,
-            )
-        elif len(db_resp.results) != 1:
-            self._logger.warning(
-                "Got %d users matching handle %s, expecting 1",
-                len(db_resp.results), request.handle)
-            return users_pb2.LoginResponse(
-                result=users_pb2.LoginResponse.ERROR,
-                error="Got wrong number of users matching query",
-            )
-        # Check password
-        if not self._check_password(request.password,
-                                    db_resp.results[0].password):
-            self._logger.info("ACCESS DENIED for user %s", request.handle)
+        try:
+            user, err = get_user_and_check_pw(self._logger,
+                                              self._db_stub,
+                                              request.handle,
+                                              request.password)
+        except ValueError as e:
+                return users_pb2.LoginResponse(
+                    result=users_pb2.LoginResponse.ERROR,
+                    error=str(e),
+                )
+
+        if err != None:
             return users_pb2.LoginResponse(
                 result=users_pb2.LoginResponse.DENIED,
             )
-        self._logger.info("*hacker voice* I'm in (%s)", request.handle)
         return users_pb2.LoginResponse(
             result=users_pb2.LoginResponse.ACCEPTED,
-            display_name=db_resp.results[0].display_name,
-            global_id=db_resp.results[0].global_id,
+            display_name=user.display_name,
+            global_id=user.global_id,
         )
 
---
+++ services/users/util.py
@@ -0,0 +1,53 @@
+import bcrypt
+from services.proto import database_pb2
+
+def check_password(logger, pw, pw_hash):
+    try:
+        return bcrypt.checkpw(pw.encode('utf-8'),
+                              pw_hash.encode('utf-8'))
+    except ValueError as e:
+        logger.warning(
+            "Password hash in DB '%s' caused an error: %s",
+            pw_hash, str(e))
+        return False # Invalid salt.
+
+def get_user_and_check_pw(logger, db_stub, handle, pw):
+    """
+    get_user_and_check finds a user and checks their password.
+
+    Raises:
+        On a database lookup failure, raises a ValueError.
+
+    Returns:
+        Returns a tuple: (Result, Error)
+        Error is a string containing details a login error.
+
+        if their password is incorrect: returns None, Error
+        if their password is correct: returns UserEntry, None
+    """
+    find_request = database_pb2.UsersRequest(
+        request_type=database_pb2.UsersRequest.FIND,
+        match=database_pb2.UsersEntry(
+            handle=handle,
+        ),
+    )
+    db_resp = db_stub.Users(find_request)
+    if db_resp.result_type != database_pb2.UsersResponse.OK:
+        err = "Error getting user from DB: " + db_resp.error
+        logger.warning(err)
+        raise ValueError(db_resp.error)
+
+    elif len(db_resp.results) != 1:
+        err = "Got {} users matching handle {}, expecting 1".format(
+              len(db_resp.results), handle)
+        logger.warning(err)
+        raise ValueError("Got wrong number of users matching query")
+
+    user = db_resp.results[0]
+    if not check_password(logger, pw, user.password):
+        err = "ACCESS DENIED for user %s", handle
+        logger.info(err)
+        return None, err
+
+    logger.info("*hacker voice* I'm in (%s)", handle)
+    return user, None
