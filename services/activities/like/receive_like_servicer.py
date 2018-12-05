6029f8b5812a30e1b71771717116d56294e19897
CianLR <cian.ruane1@gmail.com>
2018-12-05 13:08:08 +0000

    Add inital likes to DB

--- services/activities/like/receive_like_servicer.py
+++ services/activities/like/receive_like_servicer.py
@@ -1,10 +1,35 @@
+from services.proto import database_pb2 as db_pb
 from services.proto import like_pb2
 
 class ReceiveLikeServicer:
-    def __init__(self, logger):
+    def __init__(self, logger, db, user_util):
         self._logger = logger
+        self._db = db
+        self._user_util = user_util
+
+    def _get_liked_object(self, liked_obj):
+        # Case one, foreign article.
+        posts_req = db_pb.PostsRequest(
+            request_type=db_pb.PostsRequest.FIND,
+            match=db_pb.PostsEntry(
+                ap_id=liked_obj,
+            ),
+        )
+        resp = self._db.Posts(posts_req)
+        if resp.result_type != db_pb.PostsResponse.OK:
+            return None, resp.error
+        elif len(resp.results) > 1:
+            return None, "Recieved too many results from DB"
+        elif len(resp.results) == 1:
+            return resp.results[0].global_id, None
+        # Case two, local article
+        
+
 
     def ReceiveLikeActivity(self, req, context):
+        self._logger.debug("Got like for %s from %s",
+                           req.liked_object,
+                           req.liker_id)
         return like_pb2.LikeResponse(
             result_type=like_pb2.LikeResponse.ERROR,
             error="Not implemented",
--- services/database/posts_servicer.py
+++ services/database/posts_servicer.py
@@ -28,13 +28,15 @@ class PostsDatabaseServicer:
         try:
             self._db.execute(
                 'INSERT INTO posts '
-                '(author_id, title, body, creation_datetime, md_body, ap_id) '
-                'VALUES (?, ?, ?, ?, ?, ?)',
+                '(author_id, title, body, creation_datetime, '
+                'md_body, ap_id, likes_count) '
+                'VALUES (?, ?, ?, ?, ?, ?, ?)',
                 req.entry.author_id, req.entry.title,
                 req.entry.body,
                 req.entry.creation_datetime.seconds,
                 req.entry.md_body,
                 req.entry.ap_id,
+                req.entry.likes_count,
                 commit=False)
             res = self._db.execute(
                 'SELECT last_insert_rowid() FROM posts LIMIT 1')
@@ -52,7 +54,7 @@ class PostsDatabaseServicer:
         resp.global_id = res[0][0]
 
     def _db_tuple_to_entry(self, tup, entry):
-        if len(tup) != 7:
+        if len(tup) != 8:
             self._logger.warning(
                 "Error converting tuple to PostsEntry: " +
                 "Wrong number of elements " + str(tup))
@@ -66,6 +68,7 @@ class PostsDatabaseServicer:
             entry.creation_datetime.seconds = tup[4]
             entry.md_body = tup[5]
             entry.ap_id = tup[6]
+            entry.likes_count = tup[7]
         except Exception as e:
             self._logger.warning(
                 "Error converting tuple to PostsEntry: " +
