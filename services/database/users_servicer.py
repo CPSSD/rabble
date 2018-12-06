30311ff7b437c1a7153c54158d29d051faa0603f
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2018-12-06 07:13:21 +0000

    Add UPDATE to users service

--- services/database/users_servicer.py
+++ services/database/users_servicer.py
@@ -14,6 +14,7 @@ class UsersDatabaseServicer:
             database_pb2.UsersRequest.INSERT: self._users_handle_insert,
             database_pb2.UsersRequest.FIND: self._users_handle_find,
             database_pb2.UsersRequest.FIND_NOT: self._users_handle_find_not,
+            database_pb2.UsersRequest.UPDATE: self._users_handle_update,
         }
 
     def _db_tuple_to_entry(self, tup, entry):
@@ -63,6 +64,38 @@ class UsersDatabaseServicer:
             resp.result_type = database_pb2.UsersResponse.ERROR
             resp.error = str(e)
             return
+        resp.result_type = database_pb2.UsersResponse.OK
+
+    def _users_handle_update(self, req, resp):
+        update_clause, u_values = util.entry_to_update(req.entry)
+        filter_clause, f_values = util.equivalent_filter(req.match)
+        values = u_values + f_values
+
+        if not filter_clause or not update_clause:
+            self._logger.info("Could not generate update SQL: "
+                              "filter = %s, update = %s",
+                              filter_clause, update_clause)
+            resp.result_type = database_pb2.UsersResponse.ERROR
+            resp.error = str(e)
+            return
+
+        sql = "UPDATE users SET " +  update_clause  + " WHERE " +  filter_clause
+        valstr = ', '.join(str(v) for v in values)
+        self._logger.debug('Running query "%s" with values (%s)', sql, valstr)
+
+        try:
+            count = self._db.execute_count(sql, *values)
+        except sqlite3.Error as e:
+            resp.result_type = database_pb2.UsersResponse.ERROR
+            resp.error = str(e)
+            return
+
+        if count != 1:
+            err = 'UPDATE affected {} rows, expected 1'.format(count)
+            resp.result_type = database_pb2.DbFollowResponse.ERROR
+            self._logger.warning(err)
+            resp.error = err
+
         resp.result_type = database_pb2.UsersResponse.OK
 
     def _users_handle_find_not(self, req, resp):
