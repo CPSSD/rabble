d0a3f3a44caea33a5e1287ca8f7ac6928d48a937
iandioch <iandioch11@gmail.com>
2018-12-10 17:54:39 +0000

    Add db lookup to create actor items

--- services/actors/servicer.py
+++ services/actors/servicer.py
@@ -1,4 +1,4 @@
-from services.proto import actors_pb2_grpc
+from services.proto import actors_pb2, actors_pb2_grpc
 
 
 class ActorsServicer(actors_pb2_grpc.ActorsServicer):
@@ -8,5 +8,18 @@ class ActorsServicer(actors_pb2_grpc.ActorsServicer):
         self._users_util = users_util
         self._db_stub = db_stub
 
+    def _create_actor(self, username):
+        user = self._users_util.get_user_from_db(handle=username, host=None)
+        if user is None:
+            self._logger.warning('Could not find user in database.')
+            return None
+        return actors_pb2.ActorObject(
+            type='Person',
+            preferredUsername=self._users_util.parse_username(username)[0],
+            name=user.display_name,
+        )
+
     def Get(self, request, context):
-        pass
+        self._logger.debug('Actor requested for {}'.format(request.username))
+        actor = self._create_actor(request.username)
+        return actors_pb2.ActorResponse(actor=actor)
