0418265227381b8b29564ccea82e668ee9b8372a
SailSlick <sailslickcode@gmail.com>
2019-02-10 16:22:24 +0000

    Add client calls to server for search

---
+++ chump/src/models/search.ts
@@ -0,0 +1,45 @@
+import * as Promise from "bluebird";
+import * as request from "superagent";
+
+import { IParsedPost, SortPosts } from "./posts";
+
+export interface IParsedUser {
+  handle: string;
+  host: string;
+  global_id: string;
+  bio: string;
+}
+
+export interface ISearchResponse {
+  posts: IParsedPost[];
+  users: IParsedUser[];
+}
+
+export function SearchAPIPromise(endpoint: string) {
+  return new Promise((resolve, reject) => {
+    request
+      .get(endpoint)
+      .set("Accept", "application/json")
+      .retry(2)
+      .end((error, res) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        // Feed will respond with an empty response if no blogs are avaiable.
+        let body = res!.body;
+        if (body === null) {
+          body = {};
+        }
+        const posts = body!.posts || [];
+        const users = body!.users || [];
+        const parsedPosts = SortPosts(posts);
+        resolve({posts: parsedPosts, users});
+      });
+  });
+}
+
+export function Search(query: string) {
+  const endpoint: string = '/c2s/search?query="' + query + '"';
+  return SearchAPIPromise(endpoint);
+}
