b37d601fb072fee3a32358c2f2a2940504b14e0e
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2019-02-10 13:42:04 +0000

    Increase max search result

--- services/search/bleve/search.go
+++ services/search/bleve/search.go
@@ -135,7 +135,7 @@ func (s *Server) initIndex() {
 
 func (s *Server) Search(ctx context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
 	const (
-		MAX_RESULTS = 20
+		MAX_RESULTS = 50
 	)
 
 	q := bleve.NewMatchQuery(r.Query.QueryText)
