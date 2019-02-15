b355a19b53c2b23a6567e312274e9c5fb533ab9d
CianLR <cian.ruane1@gmail.com>
2019-02-15 18:29:58 +0000

    Add tests for replace_vars

--- containers/build_compose.py
+++ containers/build_compose.py
@@ -16,16 +16,15 @@ def read_template(path):
     f.close()
     return config
 
-def replace_vars(config):
+def replace_vars(config, environ):
     pattern = re.compile(r"\[\[(.+?)\]\]")  # Matches: [[something]]
     variable_pairs = {}
     # Gather all required variables.
     for match in re.finditer(pattern, config):
         var_name = "RABBLE_" + match.group(1)
-        if var_name not in os.environ:
-            print("Could not find env var", var_name)
-            sys.exit(1)
-        variable_pairs[match.group(0)] = os.environ[var_name]
+        if var_name not in environ:
+            raise ValueError("Could not find env var: " + var_name)
+        variable_pairs[match.group(0)] = environ[var_name]
     for var, value in variable_pairs.items():
         config = config.replace(var, value)
     return config
@@ -39,7 +38,7 @@ def main():
     args = get_args()
     print("Generating Rabble docker-compose config:", args.output)
     config = read_template(args.template)
-    out_config = replace_vars(config)
+    out_config = replace_vars(config, os.environ)
     write_config(out_config, args.output)
 
 if __name__ == '__main__':
