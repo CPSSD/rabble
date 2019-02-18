37ce3a59a8431a7c0e16c49c6cbcd1e475878bb7
iandioch <iandioch11@gmail.com>
2019-02-18 20:53:30 +0000

    Add db stuff

---
+++ services/database/view_servicer.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+from services.proto import database_pb2 as db_pb
+
+
+class ViewDatabaseServicer:
+    def __init__(self, db, logger):
+        self._db = db
+        self._logger = logger
+        self._logger.debug("View database servicer")
+
+    def AddView(self, req, context):
+        self._logger.debug(
+            "Adding view by %s to path %s",
+            req.user, req.path
+        )
+        response = db_pb.AddViewResponse()
+        # TODO: use user_ids instead of hosts
+        # TODO: insert datetime too
+        try:
+            pass
+            '''
+            self._db.execute(
+                'INSERT INTO views (user_id, path) '
+                'VALUES (?, ?)',
+                req.user,
+                req.path,
+                commit=True
+            )'''
+        except sqlite3.Error as e:
+            #self._db.commit()
+            self._logger.error("AddView error: %s", str(e))
+        return response
+
+
