6b7669391300bf30c31dbe83ecc6aa1c5184c354
CianLR <cian.ruane1@gmail.com>
2019-02-24 14:00:17 +0000

    Change servicer style and shutdown conn in skinny

--- services/activities/delete/servicer.py
+++ services/activities/delete/servicer.py
@@ -6,15 +6,8 @@ from receive_delete_servicer import ReceiveDeleteServicer
 
 class S2SDeleteServicer(delete_pb2_grpc.S2SDeleteServicer):
     def __init__(self, logger, db, activ_util):
-        self._logger = logger
-        self._receive_delete = ReceiveDeleteServicer(
-            logger, db, activ_util)
-        self._send_like_delete = SendLikeDeleteServicer(
-            logger, db, activ_util)
-
-    def SendLikeDeleteActivity(self, req, ctx):
-        return self._send_like_delete.SendLikeDeleteActivity(req, ctx)
-
-    def ReceiveDeleteActivity(self, req, ctx):
-        return self._receive_delete.ReceiveDeleteActivity(req, ctx)
+        send_like_delete = SendLikeDeleteServicer(logger, db, activ_util)
+        self.SendLikeDeleteActivity = send_like_delete.SendLikeDeleteActivity
+        receive_delete = ReceiveDeleteServicer(logger, db, activ_util)
+        self.ReceiveDeleteActivity = receive_delete.ReceiveDeleteActivity
 
