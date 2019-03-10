dfbf51bb375f959bd74e0c3e92a0a6a82ed76000
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2019-03-10 22:22:23 +0000

    Add basic announce handler

---
+++ chump/src/models/reblog.ts
@@ -0,0 +1,33 @@
+import * as request from "superagent";
+
+interface IReblogPostBody {
+  article_id: number;
+}
+
+export function SendReblog(articleId: number) {
+  const endpoint: string = "/c2s/announce";
+  const postBody: IReblogPostBody = {
+    article_id: articleId,
+  };
+  return new Promise((resolve, reject) => {
+    request
+      .post(endpoint)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .send(postBody)
+      .retry(2)
+      .end((error, res) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+
+        const resp = res!.body;
+        if (res.status != 200) {
+          reject(new Error("status = " + res.status.toString()))
+        }
+
+        resolve(res);
+      });
+  });
+}
