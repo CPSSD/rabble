6968d9da700de9294ba3a611bfd9fad062a068c5
CianLR <cian.ruane1@gmail.com>
2019-03-10 21:09:37 +0000

    Remove forward_like func

--- services/activities/like/receive_like_servicer.py
+++ services/activities/like/receive_like_servicer.py
@@ -35,41 +35,6 @@ class ReceiveLikeServicer:
             return resp.error
         return None
 
-    def _forward_like(self, author_id, req):
-        # Forward like to following servers.
-        self._logger.info("Sending like to followers")
-        resp = self._db.Follow(db_pb.DbFollowRequest(
-            request_type=db_pb.DbFollowRequest.FIND,
-            match=db_pb.Follow(followed=author_id),
-        ))
-        if resp.result_type != db_pb.DbFollowResponse.OK:
-            return db_pb.error
-        self._logger.info("Have %d users to notify", len(resp.results))
-        # Gather up the users, filter local and non-unique hosts.
-        hosts_to_users = {}
-        for follow in resp.results:
-            user = self._user_util.get_user_from_db(
-                global_id=follow.follower)
-            if user is None:
-                self._logger.warning(
-                    "Could not find user %d, skipping", user_id)
-                continue
-            elif not user.host:
-                continue  # Local user, already handled.
-            hosts_to_users[user.host] = user
-        # Send the activities off.
-        activity = build_like_activity(req.liker_id, req.liked_object)
-        for host, user in hosts_to_users.items():
-            inbox = self._activ_util.build_inbox_url(user.handle, host)
-            self._logger.info("Sending like to: %s", inbox)
-            resp, err = self._activ_util.send_activity(activity, inbox)
-            if err:
-                self._logger.warning(
-                    "Error sending activity to '%s' at '%s': %s",
-                    user.handle, host, str(err)
-                )
-        return None
-
     def ReceiveLikeActivity(self, req, context):
         self._logger.debug("Got like for %s from %s",
                            req.liked_object,
