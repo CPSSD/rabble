e5d752c59ff15b64239817ff0dc1163ad27d9a50
Noah Ó Donnaile <iandioch11@gmail.com>
2019-03-13 15:32:21 +0000

    add https in tests

--- services/utils/test_activities.py
+++ services/utils/test_activities.py
@@ -10,11 +10,11 @@ class ActivitiesUtilTest(unittest.TestCase):
 
     def test_build_actor(self):
         self.assertEqual(self.activ_util.build_actor('a', 'b.com'),
-                         'http://b.com/ap/@a')
+                         'https://b.com/ap/@a')
 
     def test_build_inbox_url(self):
         self.assertEqual(self.activ_util.build_inbox_url('a', 'b.com'),
-                         'http://b.com/ap/@a/inbox')
+                         'https://b.com/ap/@a/inbox')
 
     def test_send_activity_error(self):
         from urllib import request
@@ -23,14 +23,14 @@ class ActivitiesUtilTest(unittest.TestCase):
         request.urlopen.side_effect = Exception('some weird error')
         activity = {'@context': ActivitiesUtil.rabble_context(), 'data': 'yes'}
         _, e = self.activ_util.send_activity(activity,
-                                             'followed.com/ap/@b/inbox')
+                                             'https://followed.com/ap/@b/inbox')
         self.assertEqual(e, 'some weird error')
 
     def test_send_activity(self):
         from urllib import request
         request.Request = Mock()
         request.urlopen = Mock()
-        activity = {'@context': 'http://test.com', 'data': 'yes'}
+        activity = {'@context': 'https://test.com', 'data': 'yes'}
         _, e = self.activ_util.send_activity(activity,
-                                             'followed.com/ap/@b/inbox')
+                                             'https://followed.com/ap/@b/inbox')
         self.assertIsNone(e)
