b551dfd3d03d3ee65a046ff2a4c1a34c4358fc97
CianLR <cian.ruane1@gmail.com>
2019-04-02 12:04:37 +0100

    Add update service boilerplate

---
+++ services/activities/update/send_update_servicer.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+from services.proto import database_pb2 as dbpb
+from services.proto import update_pb2 as upb
+
+HOSTNAME_ENV = 'HOST_NAME'
+
+class SendUpdateServicer:
+    def __init__(self, logger, db, activ_util, users_util, hostname=None):
+        self._logger = logger
+        self._db = db
+        self._activ_util = activ_util
+        self._users_util = users_util
+        self._hostname = hostname if hostname else os.environ.get(HOSTNAME_ENV)
+        if not self._hostname:
+            self._logger.error("Hostname for SendUpdateServicer not set")
+            sys.exit(1)
+
+    def SendUpdateActivity(self, req, ctx):
+        return upb.UpdateResponse(result_type=upb.UpdateResponse.OK)
+
