e45a9db8bb3d42343aa1ec174f9b6dfed68c62a4
Aaron Delaney <aaron.delaney29@mail.dcu.ie>
2019-04-05 19:12:06 +0100

    Add instance blacklist file to gen files

--- containers/gen_first_config.sh
+++ containers/gen_first_config.sh
@@ -15,6 +15,7 @@ export RABBLE_NETWORK_NAME="testnetwork"
 export RABBLE_NETWORK_CONFIG="driver: bridge"
 export RABBLE_DBPATH="/repo/rabble.db"
 export RABBLE_EXTERNAL_ADDRESS="skinny_1:1916"
+export RABBLE_INSTANCE_BLACKLIST_FILE="/repo/config/instance_blacklist"
 
 python3 build_out/containers/build_compose.py \
   --template="build_out/containers/docker-compose.tmpl.yml" \
