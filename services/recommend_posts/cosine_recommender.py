543cb695b7b731c57795618eafa1244a97bb3234
SailSlick <sailslickcode@gmail.com>
2019-04-08 15:50:19 +0100

    Get top n recommendations

--- services/recommend_posts/cosine_recommender.py
+++ services/recommend_posts/cosine_recommender.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
+from heapq import heappush, heappushpop
 
 from services.proto import database_pb2
 from utils.recommenders import RecommendersUtil
+from utils.articles import get_article
 
 
 class CosineRecommender:
@@ -20,7 +22,8 @@ class CosineRecommender:
         self.user_tag_freq = defaultdict(int)
         self.posts = self._get_all_posts_and_tags()
         self._logger.info("post-tags: {}".format(self.posts))
-        self.user_models = self._get_all_user_models()
+        self.users = self._get_all_user()
+        self.user_models = self._create_user_models(self.users)
         self._logger.info("user_models: {}".format(self.user_models))
 
         # Calculate Inverse Frequencies
@@ -52,6 +55,19 @@ class CosineRecommender:
             }
         return posts
 
+    def _clean_user_entries(self, ues):
+        # Create an array of with length same as highest user id to allow
+        # indexing by global_id
+        highest_user_id = max(pes, key=lambda x: x.global_id).global_id + 1
+        users = [{}] * highest_user_id
+        for ue in ues:
+            likes = self._clean_likes(ue.likes)
+            users[ue.global_id] = {
+                "global_id": ue.global_id,
+                "likes": likes
+            }
+        return users
+
     def _clean_likes(self, likes):
         # The GROUP_CONCAT method in sqlite joins objects with "," into a string
         return [int(x) for x in likes.split(",") if x != ""]
@@ -61,7 +77,7 @@ class CosineRecommender:
         # model
         user_models = defaultdict(lambda: defaultdict(int))
         for u in users:
-            for post_id in self._clean_likes(u.likes):
+            for post_id in u.likes:
                 for tag in self.posts[post_id]["tags"]:
                     self.user_tag_freq[tag] += 1
                     user_models[u.global_id][tag] += 1
@@ -75,26 +91,24 @@ class CosineRecommender:
             return []
         return self._clean_post_entries(find_resp.results)
 
-    def _get_all_user_models(self):
+    def _get_all_user(self):
         find_resp = self._db.AllUserLikes(database_pb2.AllUsersRequest())
         if find_resp.result_type == database_pb2.UsersResponse.ERROR:
             self._logger.error(
                 'Error getting AllUserLikes for Cosine: {}'.format(find_resp.error))
             return []
 
-        return self._create_user_models(find_resp.results)
+        return self._clean_user_entries(find_resp.results)
 
     def _tf_idf_cosine_similarity(self, user_model, post_tags):
         sum_user_item_tf = 0
         sum_user_tf = 0
         sum_item_tf = 0
-        tag_amount = 0
         for tag in post_tags:
             sum_user_item_tf += user_model[tag] * \
                 self.user_tag_ifs[tag] * self.post_tag_ifs[tag]
             sum_user_tf += (user_model[tag] * self.user_tag_ifs[tag]) ** 2
             sum_item_tf += self.post_tag_ifs[tag] ** 2
-            tag_amount += 1
         divisor = (((sum_user_tf) ** 0.5) * ((sum_item_tf) ** 0.5))
         if divisor == 0:
             return 0
@@ -102,8 +116,29 @@ class CosineRecommender:
         return tf_cosine
 
     def get_recommendations(self, user_id, n):
-        user_model = self.user_models[user_id]
-        if user_model == {}:
+        u_m = self.user_models[user_id]
+        if u_m == {}:
             self._logger.info(
                 'Cosine user_model is empty. id: {}'.format(user_id))
             return [], None
+
+        # Calculate similarities
+        sims = []
+        for p in self.posts:
+            # do not recommend liked posts
+            if p["global_id"] in self.users[user_id]["likes"]:
+                continue
+            sim = self._tf_idf_cosine_similarity(u_m, p["tags"])
+            if len(sims) < n:
+                heappush(sims, (sim, p["global_id"]))
+            else:
+                heappushpop(sims, (sim, p["global_id"]))
+
+        # get top n results
+        sims = sorted(sims, reverse=True)
+        self._logger.info('Recommended (score, id): {}'.format(sims))
+        posts_entries = []
+        for result in sims:
+            art = get_article(self._logger, self._db, global_id=results[1])
+            posts_entries.append(art)
+        return posts_entries
