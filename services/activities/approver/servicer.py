be8f7f07e174e417dc9334ef500b9af2e94f1b3b
Noah Ó Donnaile <iandioch11@gmail.com>
2019-04-08 14:51:44 +0000

    Add sender_id to approver

--- services/activities/approver/servicer.py
+++ services/activities/approver/servicer.py
@@ -6,7 +6,7 @@ class ApproverServicer(approver_pb2_grpc.ApproverServicer):
     def __init__(self, logger, db_stub, activ_util, users_util):
         self._logger = logger
 
-        send = SendApprovalServicer(logger, activ_util)
+        send = SendApprovalServicer(logger, activ_util, users_util)
         self.SendApproval = send.SendApproval
         receive = ReceiveApprovalServicer(logger, db_stub, users_util)
         self.ReceiveApproval = receive.ReceiveApproval
