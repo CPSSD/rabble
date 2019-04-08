d41735d8a22af9af33e6b2077881acdb7fe679b3
CianLR <cian.ruane1@gmail.com>
2019-04-08 10:50:59 +0100

    Move back in history on post delete

--- chump/src/components/single_post.tsx
+++ chump/src/components/single_post.tsx
@@ -16,6 +16,9 @@ interface ISinglePostProps extends RouteProps {
       user: string,
     },
   };
+  history: {
+    goBack: () => void;
+  }
   username: string;
 }
 
@@ -60,6 +63,7 @@ export class SinglePost extends RootComponent<ISinglePostProps, ISinglePostState
           blogPost={this.state.posts[0]}
           preview={false}
           customCss={true}
+          goBack={this.props.history.goBack}
         />
       </div>
     );
