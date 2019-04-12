8ec81f5614ae8b6563330d9859102226ac9c4b4f
CianLR <cian.ruane1@gmail.com>
2019-04-12 15:07:43 +0100

    Fix double space date in Post

--- chump/src/components/post.tsx
+++ chump/src/components/post.tsx
@@ -73,8 +73,7 @@ export class Post extends RootComponent<IPostProps, IPostState> {
       <div className="pure-u-10-24">
         {customStyle}
         <p className="article-byline">
-          {config.published} &nbsp;
-          {post.parsed_date.toLocaleString()}
+          {`${config.published} ${post.parsed_date.toLocaleString()}`}
         </p>
         <Link
           to={`/@${post.author}/${post.global_id}`}
